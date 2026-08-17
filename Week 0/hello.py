306c73da08127a9de55ed06205c3136c9a3093fc
ivan.kyrylko <ivan.kyrylko.dev@gmail.com>
2026-08-17 21:37:39 +0300

    Updated the hello.py file during the lecture.

--- Week 0/hello.py
+++ Week 0/hello.py
@@ -1,5 +1,8 @@
-# Ask the user for their name
-name = input("What's your name? ")
+# Ask the user for their name, remove whitespace from str and capitalize user's name
+name = input("What's your name? ").strip().title()
+
+# Split user's name into first name and last name
+first, last = name.split(" ")
 
 # Say hello to the user
-print("hello, " + name)
+print(f"hello, {first}")
